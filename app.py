f54a90992b44090d69b4e090ae6ba3979e559ee6
Mambaul Hisam <hisyamsquartz@gmail.com>
2024-07-08 01:06:55 +0700

    model installment

--- app.py
+++ app.py
@@ -8,38 +8,48 @@ from keras.preprocessing import image
 from PIL import Image
 import io
 import numpy as np
+from keras.utils import img_to_array
+from tensorflow.keras.layers import MaxPooling2D,Conv2D,Input,Add,MaxPool2D,Flatten,AveragePooling2D,Dense,BatchNormalization,ZeroPadding2D,Activation,Concatenate,UpSampling2D
+from tensorflow.keras.models import Model
+
+
+classes = ['ba', 'ca', 'da', 'dha', 'ga', 'ha', 'ja', 'ka', 'la', 'ma',
+           'na', 'nga', 'nya', 'pa', 'ra', 'sa', 'ta', 'tha', 'wa', 'ya']
 
 UPLOAD_FOLDER = './upload'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-model = keras.models.load_model('base_model.h5')
+model = keras.models.load_model('./model_91.keras')
 
-# def predict_label(img_stream: bytes):
-#     img = Image.open(img_stream).resize((224, 224)).convert('L')
-#     # img.show()
-#     # print(img)
-# 	# i = image.load_img(img_path, target_size=(224,224))
-#     i = image.img_to_array(img)/255.0
-#     # print(i.shape)
-#     i = i.reshape(224,224, 1)
-#     p = model.predict(i)
-#     classes_x=np.argmax(p,axis=1)
-#     return dic[classes_x[0]]
-# # def load():
+def predict(img_buf):
+    if img_buf:
+        img_pil = Image.open(io.BytesIO(img_buf)).convert('RGB').resize(size=(224, 224))
+    # img_krs = load_img('./contoh.png', color_mode='grayscale', target_size=(224, 224))
+    # img_krs.show()
+        img_arr = img_to_array(img_pil)
+        x = np.expand_dims(img_arr, axis=0)
+
+        test_img = np.vstack([x])
+    # print(test_img)
+        result = model.predict(test_img, batch_size=8)
+    # print(image_path)
+        return classes[np.argmax(result)]
+    return "None"
+
 
 
 @app.route('/', methods=['POST', 'GET'])
 def upload_file():
+    img_buf = None
     data_gambar = None
     if request.method == 'POST':
         f = request.files['file'] 
-        # f.save(f.filename)
-        # print(f.filename)
         stream = f.stream
-        data_gambar = base64.b64encode(stream.read()).decode()
-    return render_template('index.html', data=data_gambar, hasil='ha')
+        img_buf = stream.read()
+        data_gambar = base64.b64encode(img_buf).decode()
+    return render_template('index.html', data=data_gambar, hasil=predict(img_buf))
 
 if __name__ == '__main__':
     app.run()
